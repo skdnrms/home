f70f9f4600ebe9db8f267a8d87e76cb35875efc2
woogeun <skdnrms@gmail.com>
2026-01-12 00:11:10 +0900

    정적페이지 빌드 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    output: 'export',
+    basePath: '/home',        // repo 이름
+    assetPrefix: '/home/',
+    images: {
+      unoptimized: true,      // static export 필수
+    },
 };
 
 export default nextConfig;
